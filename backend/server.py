da2aa70734dc56f5c0f1cec31aafedb4d5d3bcbc
Josh Muszka <jmuszka@uwo.ca>
2026-01-17 12:16:00 -0500

    feat: search endpoint and boilerplate backend

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from dto.search import SearchRequest
+from enums.sort import SortBy
 
 app = FastAPI()
 
@@ -6,6 +8,19 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello from FastAPI running with uv!"}
 
+@app.get("/search")
+def search(
+    req: SearchRequest,
+    limit: int = Query(default=10, ge=1, le=100),
+    sort_order: str = Query(default=SortBy.RELEVANCE)
+):
+    return {
+            "message": req.query,
+            "limit": limit,
+            "sort_order": sort_order
+    }
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run("server:app", host="0.0.0.0", port=8080, reload=True)
