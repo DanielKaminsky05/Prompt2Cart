0d69e2457d1d7259e7912c7eef157d1ff8addf73
Josh Muszka <jmuszka@uwo.ca>
2026-01-17 11:44:02 -0500

    feat: initialized fastapi

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,11 @@
-##Flask or FastAPI Server
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI running with uv!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
